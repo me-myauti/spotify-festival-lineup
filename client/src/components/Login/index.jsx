3a2ef6604d52d12d51e7cb67a6100f1bd1ab4437
me-myauti <myauti.rafael@outlook.com>
2022-12-09 03:17:08 -0400

    Removed unnecessary scopes

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import  './login.css'
-const AUTH_URL ="https://accounts.spotify.com/authorize?client_id=709bc4074c7c424d8e98410ec4ede1ed&response_type=code&redirect_uri=http://localhost:5173/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20user-read-recently-played"
+const AUTH_URL ="https://accounts.spotify.com/authorize?client_id=709bc4074c7c424d8e98410ec4ede1ed&response_type=code&redirect_uri=http://localhost:5173/&scope=user-top-read%20user-read-recently-played"
 
 
 export default function Login(){
